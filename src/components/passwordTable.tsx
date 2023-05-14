28c27a78b10c55e469589b12bcda5200ebbbd8d2
Quint Legius <quint.legius@gmail.com>
2023-05-14 20:18:52 +0200

    changed css

--- src/components/passwordTable.tsx
+++ src/components/passwordTable.tsx
@@ -1,52 +1,61 @@
-import { Box } from "@mui/material";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { Alert, AlertTitle, Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
+    field: "passwordName",
+    headerName: "Name",
     width: 150,
-    editable: true,
+    editable: false,
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: "userName",
+    headerName: "Username",
     width: 150,
-    editable: true,
+    editable: false,
   },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
+    field: "password",
+    headerName: "Password",
     width: 110,
-    editable: true,
+    editable: false,
   },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
+
 ];
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, passwordName: "Snow", userName: "Jon", password: "hjdshgh" },
+  { id: 2, passwordName: "Lannister", userName: "Cersei", password: "hroeih" },
+  { id: 3, passwordName: "Lannister", userName: "Jaime", password: "jkdsbvkd" },
+  { id: 4, passwordName: "Stark", userName: "Arya", password: "FDSGGW12341" },
+  { id: 5, passwordName: "Targaryen", userName: "Daenerys", password: "hfhuo23" },
+  { id: 6, passwordName: "Melisandre", userName: null, password: "fqjehvjabqv" },
+  { id: 7, passwordName: "Clifford", userName: "Ferrara", password: "dslbvgjdb" },
+  { id: 8, passwordName: "Frances", userName: "Rossini", password: "sdkhgown" },
+  { id: 9, passwordName: "Roxie", userName: "Harvey", password: "hfslkhvl" },
 ];
 export default function PasswordTable() {
+  const [copiedData, setCopiedData] = useState('');
+
   return (
-    <Box sx={{ height: 400, width: "100%" }}>
+    <div style={{}}>
+      <Alert severity="info" sx={{ width: '100%', mt: 1 }}>
+        <AlertTitle>Copied data:</AlertTitle>
+        <code
+          style={{
+            display: 'block',
+            maxHeight: 200,
+            overflow: 'auto',
+            whiteSpace: 'pre-line',
+          }}
+        >
+          {copiedData}
+        </code>
+      </Alert>
+    <Box sx={{ height: 400,width: "70%", margin: "auto", display: "flex", alignItems: "center", justifyContent: "center"}}>
       <DataGrid
         rows={rows}
+        onClipboardCopy={(copiedString) => setCopiedData(copiedString)}
         columns={columns}
         initialState={{
           pagination: {
@@ -60,5 +69,6 @@ export default function PasswordTable() {
         disableRowSelectionOnClick
       />
     </Box>
+    </div>
   );
 }
